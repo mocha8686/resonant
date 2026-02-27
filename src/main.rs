3471e7302f1bf84eea10f9a3dfec304ad89c49d2
mocha8686 <june.mocha8686@gmail.com>
2026-02-26 19:17:24 -0800

    feat: dynamic tracks

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,48 @@
+use iced::{Element, widget::{Column, button, row}};
 use resonant::track::{self, Track};
-use iced::widget::{Column, column};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum Message {
-    Track(track::Message, u64),
+    Track(track::Message, usize),
+    AddTrack,
+    RemoveTrack,
 }
 
 #[derive(Default)]
 struct State {
-    track1: Track,
-    track2: Track,
+    tracks: Vec<Track>,
 }
 
 impl State {
     fn update(&mut self, msg: Message) {
         match msg {
-            Message::Track(msg, id) => {
-                if id == 1 {
-                    self.track1.update(msg);
-                } else {
-                    self.track2.update(msg);
-                }
-            },
+            Message::Track(msg, index) => {
+                let Some(track) = self.tracks.get_mut(index) else {
+                    return;
+                };
+                track.update(msg);
+            }
+            Message::AddTrack => self.tracks.push(Track::default()),
+            Message::RemoveTrack => {
+                self.tracks.pop();
+            }
         }
     }
 
-    fn view(&self) -> Column<'_, Message> {
-        column![
-            self.track1.view().map(|m| Message::Track(m, 1)),
-            self.track2.view().map(|m| Message::Track(m, 2)),
-        ]
+    fn view(&self) -> Element<'_, Message> {
+        let tracks = Column::from_vec(
+            self.tracks
+                .iter()
+                .enumerate()
+                .map(|(index, track)| track.view().map(move |msg| Message::Track(msg, index)))
+                .collect(),
+        );
+
+        row![
+            tracks,
+            button("+").on_press(Message::AddTrack),
+            button("-").on_press(Message::RemoveTrack),
+        ].into()
     }
 }
 
