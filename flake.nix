f3cb26889f21a818cd48f9902a65deb5b4bb9cd1
mocha8686 <june.mocha8686@gmail.com>
2026-04-13 15:11:16 -0700

    build: flake

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Rust dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            (rust-bin.selectLatestNightlyWith
+              (toolchain: toolchain.default))
+
+            bacon
+          ];
+
+          shellHook = ''
+            # shell hook
+          '';
+        };
+      }
+    );
+}
